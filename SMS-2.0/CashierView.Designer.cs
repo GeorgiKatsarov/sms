88066f83b0bbfc980a035128f103a1e9e16598ef
GeorgiKatsarov <100018528+GeorgiKatsarov@users.noreply.github.com>
2023-02-20 08:32:16 +0200

    created the cashier view form frontend

---
+++ SMS-2.0/CashierView.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace SMS_2._0
+{
+    partial class CashierView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CashierView));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.quantityDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.priceDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.stockBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.smsDataSet = new SMS_2._0.smsDataSet();
+            this.smsDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.stockTableAdapter = new SMS_2._0.smsDataSetTableAdapters.stockTableAdapter();
+            this.finaliseButton = new System.Windows.Forms.Button();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.idTXTBOX = new System.Windows.Forms.TextBox();
+            this.quantityTXTBOX = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.stockBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.smsDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.smsDataSetBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.nameDataGridViewTextBoxColumn,
+            this.quantityDataGridViewTextBoxColumn,
+            this.priceDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.stockBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(283, 44);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(530, 485);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "id";
+            this.idDataGridViewTextBoxColumn.MinimumWidth = 8;
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            this.idDataGridViewTextBoxColumn.ReadOnly = true;
+            this.idDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "name";
+            this.nameDataGridViewTextBoxColumn.MinimumWidth = 8;
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            this.nameDataGridViewTextBoxColumn.ReadOnly = true;
+            this.nameDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // quantityDataGridViewTextBoxColumn
+            // 
+            this.quantityDataGridViewTextBoxColumn.DataPropertyName = "quantity";
+            this.quantityDataGridViewTextBoxColumn.HeaderText = "quantity";
+            this.quantityDataGridViewTextBoxColumn.MinimumWidth = 8;
+            this.quantityDataGridViewTextBoxColumn.Name = "quantityDataGridViewTextBoxColumn";
+            this.quantityDataGridViewTextBoxColumn.ReadOnly = true;
+            this.quantityDataGridViewTextBoxColumn.Width = 120;
+            // 
+            // priceDataGridViewTextBoxColumn
+            // 
+            this.priceDataGridViewTextBoxColumn.DataPropertyName = "price";
+            this.priceDataGridViewTextBoxColumn.HeaderText = "price";
+            this.priceDataGridViewTextBoxColumn.MinimumWidth = 8;
+            this.priceDataGridViewTextBoxColumn.Name = "priceDataGridViewTextBoxColumn";
+            this.priceDataGridViewTextBoxColumn.ReadOnly = true;
+            this.priceDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // stockBindingSource
+            // 
+            this.stockBindingSource.DataMember = "stock";
+            this.stockBindingSource.DataSource = this.smsDataSet;
+            // 
+            // smsDataSet
+            // 
+            this.smsDataSet.DataSetName = "smsDataSet";
+            this.smsDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // smsDataSetBindingSource
+            // 
+            this.smsDataSetBindingSource.DataSource = this.smsDataSet;
+            this.smsDataSetBindingSource.Position = 0;
+            // 
+            // stockTableAdapter
+            // 
+            this.stockTableAdapter.ClearBeforeFill = true;
+            // 
+            // finaliseButton
+            // 
+            this.finaliseButton.Location = new System.Drawing.Point(18, 245);
+            this.finaliseButton.Name = "finaliseButton";
+            this.finaliseButton.Size = new System.Drawing.Size(248, 44);
+            this.finaliseButton.TabIndex = 1;
+            this.finaliseButton.Text = "Finilase sale";
+            this.finaliseButton.UseVisualStyleBackColor = true;
+            // 
+            // AddButton
+            // 
+            this.AddButton.Location = new System.Drawing.Point(21, 186);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(248, 44);
+            this.AddButton.TabIndex = 2;
+            this.AddButton.Text = "Add product";
+            this.AddButton.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(83, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 32);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "id:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 32);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "quantity:";
+            // 
+            // idTXTBOX
+            // 
+            this.idTXTBOX.Location = new System.Drawing.Point(130, 80);
+            this.idTXTBOX.Name = "idTXTBOX";
+            this.idTXTBOX.Size = new System.Drawing.Size(139, 40);
+            this.idTXTBOX.TabIndex = 5;
+            // 
+            // quantityTXTBOX
+            // 
+            this.quantityTXTBOX.Location = new System.Drawing.Point(130, 126);
+            this.quantityTXTBOX.Name = "quantityTXTBOX";
+            this.quantityTXTBOX.Size = new System.Drawing.Size(139, 40);
+            this.quantityTXTBOX.TabIndex = 6;
+            // 
+            // CashierView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 32F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(825, 541);
+            this.Controls.Add(this.quantityTXTBOX);
+            this.Controls.Add(this.idTXTBOX);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.AddButton);
+            this.Controls.Add(this.finaliseButton);
+            this.Controls.Add(this.dataGridView1);
+            this.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "CashierView";
+            this.Text = "CashierView";
+            this.Load += new System.EventHandler(this.CashierView_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.stockBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.smsDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.smsDataSetBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource smsDataSetBindingSource;
+        private smsDataSet smsDataSet;
+        private System.Windows.Forms.BindingSource stockBindingSource;
+        private smsDataSetTableAdapters.stockTableAdapter stockTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn quantityDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn priceDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Button finaliseButton;
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox idTXTBOX;
+        private System.Windows.Forms.TextBox quantityTXTBOX;
+    }
+}
---
+++ SMS-2.0/CashierView.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SMS_2._0
+{
+    public partial class CashierView : Form
+    {
+        public CashierView()
+        {
+            InitializeComponent();
+        }
+
+        private void CashierView_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'smsDataSet.stock' table. You can move, or remove it, as needed.
+            this.stockTableAdapter.Fill(this.smsDataSet.stock);
+
+        }
+
+        
+    }
+}
